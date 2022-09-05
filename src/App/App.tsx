b8eb6cf44a4bc59d6bdaa907b4c9c5859e233532
olegPavlo <112747192+olegPavlo@users.noreply.github.com>
2022-09-05 14:46:32 +0300

    commit-4

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,9 +2,9 @@ import React, { FC, useEffect, useState } from 'react'
 import app from './App.module.css'
 
 
-import { IUser } from './types/types'
-import Header from './components/Header/Header'
-import Home from './page/Home/Home'
+import { IUser } from '../types/types'
+import Header from '../components/Header/Header'
+import Home from '../page/Home/Home'
 
 const App = () => {
 
